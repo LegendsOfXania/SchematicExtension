56a5d78c407168acfef976f6c3702e9499592b87
LegendsOfXania <legendsofxania@gmail.com>
2025-12-30 19:42:28 +0100

    Initial commit

---
+++ extension/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    /* Kotlin */
+    kotlin("jvm") version "2.2.10"
+    /* Typewriter */
+    id("com.typewritermc.module-plugin") version "2.1.0"
+}
+
+group = "fr.legendsofxania"
+version = "0.0.1"
+
+repositories {}
+dependencies {}
+
+typewriter {
+    namespace = "legendsofxania"
+
+    extension {
+        name = "Schematic"
+        shortDescription = "Display schematics in Typewriter."
+        description = """
+            Display schematics in your interactions and create
+            beautiful places directly in Typewriter.
+            Created by the Legends of Xania.
+            """.trimMargin()
+        engineVersion = "0.9.0-beta-167"
+        channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
+
+        paper()
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
